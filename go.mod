0c657699144878540269492a855bf2eb531e1ba0
Paul Laffitte <paul.laffitte@enix.fr>
2021-07-08 11:28:09 +0200

    feat(cache): cache and delete images

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module gitlab.enix.io/products/docker-cache-registry
 go 1.15
 
 require (
+	github.com/google/go-containerregistry v0.5.1
 	k8s.io/api v0.20.1
 	k8s.io/apimachinery v0.20.1
 	k8s.io/client-go v0.20.1
--- internal/registry/registry.go
+++ internal/registry/registry.go
@@ -1,12 +1,74 @@
 package registry
 
-// type Registry struct {
-// }
+import (
+	"errors"
 
-func DeleteImage(image string) error {
-	return nil
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+)
+
+func imageExists(ref name.Reference) bool {
+	_, err := remote.Head(ref)
+	if err != nil {
+		return false
+	}
+
+	return true
 }
 
-func CacheImage(image string) (bool, error) {
+func DeleteImage(imageName string) error {
+	ref, err := name.ParseReference("185.145.250.158.nip.io/" + imageName)
+	if err != nil {
+		return err
+	}
+
+	if !imageExists(ref) {
+		return nil
+	}
+
+	descriptor, err := remote.Head(ref)
+	if err != nil {
+		return err
+	}
+
+	digest, err := name.NewDigest("185.145.250.158.nip.io/" + imageName + "@" + descriptor.Digest.String())
+
+	if err != nil {
+		return err
+	}
+
+	return remote.Delete(digest)
+}
+
+func CacheImage(imageName string) (bool, error) {
+	destRef, err := name.ParseReference("185.145.250.158.nip.io/" + imageName)
+	if err != nil {
+		return false, err
+	}
+	sourceRef, err := name.ParseReference(imageName)
+	if err != nil {
+		return false, err
+	}
+
+	if imageExists(destRef) {
+		return false, nil
+	}
+	if !imageExists(sourceRef) {
+		return false, errors.New("could not find source image")
+	}
+
+	image, err := remote.Image(sourceRef)
+	if err != nil {
+		return false, err
+	}
+
+	if err := remote.Write(destRef, image); err != nil {
+		return false, err
+	}
+
+	if err := remote.Put(destRef, image); err != nil {
+		return false, err
+	}
+
 	return true, nil
 }
