85a34a6a05b1906d5d220a3cd23899b4c0d676ef
Paul Laffitte <paul.laffitte@enix.fr>
2021-10-22 14:56:41 +0200

    chore(image-rewriter): initial mutating webhook

---
+++ api/v1/pod_webhook.go
@@ -0,0 +1,45 @@
+package v1
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+
+	corev1 "k8s.io/api/core/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
+)
+
+// +kubebuilder:webhook:path=/mutate-core-v1-pod,mutating=true,failurePolicy=fail,sideEffects=None,groups=core,resources=pods,verbs=create;update,versions=v1,name=mpod.kb.io,admissionReviewVersions={v1,v1beta1}
+
+type ImageRewriter struct {
+	Client  client.Client
+	decoder *admission.Decoder
+}
+
+func (a *ImageRewriter) Handle(ctx context.Context, req admission.Request) admission.Response {
+	pod := &corev1.Pod{}
+
+	err := a.decoder.Decode(req, pod)
+	if err != nil {
+		return admission.Errored(http.StatusBadRequest, err)
+	}
+
+	if pod.Annotations == nil {
+		pod.Annotations = map[string]string{}
+	}
+	pod.Annotations["example-mutating-admission-webhook"] = "foo"
+
+	marshaledPod, err := json.Marshal(pod)
+	if err != nil {
+		return admission.Errored(http.StatusInternalServerError, err)
+	}
+
+	return admission.PatchResponseFromRaw(req.Object.Raw, marshaledPod)
+}
+
+// InjectDecoder injects the decoder
+func (a *ImageRewriter) InjectDecoder(d *admission.Decoder) error {
+	a.decoder = d
+	return nil
+}
--- cmd/cache/main.go
+++ cmd/cache/main.go
@@ -25,14 +25,17 @@ import (
 	// to ensure that exec-entrypoint and run can make use of them.
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 
-	dcrenixiov1alpha1 "gitlab.enix.io/products/docker-cache-registry/api/v1alpha1"
-	"gitlab.enix.io/products/docker-cache-registry/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+
+	dcrenixiov1 "gitlab.enix.io/products/docker-cache-registry/api/v1"
+	dcrenixiov1alpha1 "gitlab.enix.io/products/docker-cache-registry/api/v1alpha1"
+	"gitlab.enix.io/products/docker-cache-registry/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -95,6 +98,7 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Pod")
 		os.Exit(1)
 	}
+	mgr.GetWebhookServer().Register("/mutate-core-v1-pod", &webhook.Admission{Handler: &dcrenixiov1.PodAnnotator{Client: mgr.GetClient()}})
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
