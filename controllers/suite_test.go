4b98c4b206a6e9706ceeff42ff31e560e37dcb6b
Paul Laffitte <paul.laffitte@enix.fr>
2023-01-13 17:24:58 +0100

    chore(tests): always write application logs to stdout

--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -101,7 +101,7 @@ func removeRegistry() {
 }
 
 var _ = BeforeSuite(func(done Done) {
-	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
+	logf.SetLogger(zap.New(zap.WriteTo(os.Stdout), zap.UseDevMode(true)))
 
 	By("bootstrapping test environment")
 	testEnv = &envtest.Environment{
