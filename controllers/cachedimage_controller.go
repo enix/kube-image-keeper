enix/kube-image-keeper

commit 937b568cf8717e9fcc8fc0cbb14f12f25bdfc8dd

fix(controllers): properly update usedBy CachedImage status
When deleting CachedImage in use, at the time it was recreated, its usedBy status wasn't updated. Updating the status from the CachedImage controller instead of the Pod controller fixed it as the status is now updated each time a CachedImage is updated.

controllers/cachedimage_controller.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/klog/v2"
@@ -16,6 +17,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/enix/kube-image-keeper/api/v1alpha1"
 	kuikenixiov1alpha1 "github.com/enix/kube-image-keeper/api/v1alpha1"
 	"github.com/enix/kube-image-keeper/internal/registry"
 )
@@ -135,12 +137,20 @@ func (r *CachedImageReconciler) Reconcile(ctx context.Context, req ctrl.Request)
 	}
 
 	// Update CachedImage IsCached status
+	log.Info("updating CachedImage status")
 	cachedImage.Status.IsCached = true
 	err = r.Status().Update(context.Background(), &cachedImage)
 	if err != nil {
 		if statusErr, ok := err.(*errors.StatusError); ok && statusErr.Status().Code == http.StatusConflict {
 			return ctrl.Result{Requeue: true}, nil
 		}
+		return ctrl.Result{}, err
+	}
+
+	// Update CachedImage UsedBy status
+	if requeue, err := r.updatePodCount(ctx, &cachedImage); requeue {
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
 		return ctrl.Result{}, err
 	}
 
@@ -177,6 +187,37 @@ func (r *CachedImageReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&kuikenixiov1alpha1.CachedImage{}).
 		Complete(r)
+}
+
+// updatePodCount update CachedImage UsedBy status
+func (r *CachedImageReconciler) updatePodCount(ctx context.Context, cachedImage *kuikenixiov1alpha1.CachedImage) (requeue bool, err error) {
+	var podsList corev1.PodList
+	if err = r.List(ctx, &podsList, client.MatchingFields{cachedImageOwnerKey: cachedImage.Name}); err != nil && !apierrors.IsNotFound(err) {
+		return
+	}
+
+	pods := []v1alpha1.PodReference{}
+	for _, pod := range podsList.Items {
+		if !pod.DeletionTimestamp.IsZero() {
+			continue
+		}
+		pods = append(pods, v1alpha1.PodReference{NamespacedName: pod.Namespace + "/" + pod.Name})
+	}
+
+	cachedImage.Status.UsedBy = v1alpha1.UsedBy{
+		Pods:  pods,
+		Count: len(pods),
+	}
+
+	err = r.Status().Update(context.Background(), cachedImage)
+	if err != nil {
+		if statusErr, ok := err.(*errors.StatusError); ok && statusErr.Status().Code == http.StatusConflict {
+			requeue = true
+		}
+		return
+	}
+
+	return
 }
 
 // Helper functions to check and remove string from a slice of strings.

controllers/pod_controller.go
@@ -13,7 +13,6 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 
 	"github.com/docker/distribution/reference"
-	"github.com/enix/kube-image-keeper/api/v1alpha1"
 	kuikenixiov1alpha1 "github.com/enix/kube-image-keeper/api/v1alpha1"
 	"github.com/enix/kube-image-keeper/internal/registry"
 	corev1 "k8s.io/api/core/v1"
@@ -109,11 +108,6 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 					return ctrl.Result{}, err
 				}
 			}
-
-			err = r.updatePodCount(ctx, &ci)
-			if err != nil {
-				return ctrl.Result{}, err
-			}
 		}
 
 		log.Info("removing finalizer")
@@ -160,9 +154,6 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 			ci.Spec = cachedImage.Spec
 
 			if err = r.Patch(ctx, &ci, patch); err != nil {
-				return ctrl.Result{}, err
-			}
-			if err = r.updatePodCount(ctx, &ci); err != nil {
 				return ctrl.Result{}, err
 			}
 		}
@@ -233,34 +224,6 @@ func (r *PodReconciler) podsWithDeletingCachedImages(obj client.Object) []ctrl.R
 	}
 
 	return make([]ctrl.Request, 0)
-}
-
-// updatePodCount update CachedImage UsedBy status
-func (r *PodReconciler) updatePodCount(ctx context.Context, cachedImage *kuikenixiov1alpha1.CachedImage) error {
-	var podsList corev1.PodList
-	if err := r.List(ctx, &podsList, client.MatchingFields{cachedImageOwnerKey: cachedImage.Name}); err != nil && !apierrors.IsNotFound(err) {
-		return err
-	}
-
-	pods := []v1alpha1.PodReference{}
-	for _, pod := range podsList.Items {
-		if !pod.DeletionTimestamp.IsZero() {
-			continue
-		}
-		pods = append(pods, v1alpha1.PodReference{NamespacedName: pod.Namespace + "/" + pod.Name})
-	}
-
-	patch := client.MergeFrom(cachedImage.DeepCopy())
-	cachedImage.Status.UsedBy = v1alpha1.UsedBy{
-		Pods:  pods,
-		Count: len(pods),
-	}
-
-	if err := r.Status().Patch(context.Background(), cachedImage, patch); err != nil {
-		return client.IgnoreNotFound(err)
-	}
-
-	return nil
 }
 
 func desiredCachedImages(ctx context.Context, pod *corev1.Pod) []kuikenixiov1alpha1.CachedImage {

commit e13e679ed7480eeb39b61a241077957480ccfc60

fix(controllers): fix pod count when scaling up from a >0 number of pods
regression from 937b568cf8717e9fcc8fc0cbb14f12f25bdfc8dd

controllers/cachedimage_controller.go
@@ -205,18 +205,29 @@ func (r *CachedImageReconciler) SetupWithManager(mgr ctrl.Manager, maxConcurrent
 		return err
 	}
 
-	p := predicate.Funcs{
-		DeleteFunc: func(e event.DeleteEvent) bool {
-			return true
-		},
-	}
-
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&kuikenixiov1alpha1.CachedImage{}).
 		Watches(
 			&source.Kind{Type: &corev1.Pod{}},
-			handler.EnqueueRequestsFromMapFunc(r.cachedImagesWithDeletingPods),
-			builder.WithPredicates(p),
+			handler.EnqueueRequestsFromMapFunc(r.cachedImagesRequestFromPod),
+			builder.WithPredicates(predicate.Funcs{
+				DeleteFunc: func(e event.DeleteEvent) bool {
+					pod := e.Object.(*corev1.Pod)
+					var currentPod corev1.Pod
+					// wait for the Pod to be really deleted
+					err := r.Get(context.Background(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, &currentPod)
+					return err != nil && apierrors.IsNotFound(err)
+				},
+			}),
+		).
+		Watches(
+			&source.Kind{Type: &corev1.Pod{}},
+			handler.EnqueueRequestsFromMapFunc(r.cachedImagesRequestFromPod),
+			builder.WithPredicates(predicate.Funcs{
+				CreateFunc: func(e event.CreateEvent) bool {
+					return true
+				},
+			}),
 		).
 		WithOptions(controller.Options{
 			MaxConcurrentReconciles: maxConcurrentReconciles,
@@ -265,19 +276,13 @@ func containsString(slice []string, s string) bool {
 	return false
 }
 
-func (r *CachedImageReconciler) cachedImagesWithDeletingPods(obj client.Object) []ctrl.Request {
+func (r *CachedImageReconciler) cachedImagesRequestFromPod(obj client.Object) []ctrl.Request {
 	log := log.
 		FromContext(context.Background()).
 		WithName("controller-runtime.manager.controller.cachedImage.deletingPods").
 		WithValues("pod", klog.KObj(obj))
 
 	pod := obj.(*corev1.Pod)
-	var currentPod corev1.Pod
-	// wait for the Pod to be really deleted
-	if err := r.Get(context.Background(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, &currentPod); err == nil || !apierrors.IsNotFound(err) {
-		return make([]ctrl.Request, 0)
-	}
-
 	ctx := logr.NewContext(context.Background(), log)
 	cachedImages := desiredCachedImages(ctx, pod)
 