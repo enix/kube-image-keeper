0acf3f0d20ad48010a7a9eb50f75d920b516aace
Paul Laffitte <paul.laffitte@enix.fr>
2023-12-01 16:13:55 +0100

    test(proxy): fix /v2 endpoint test

--- internal/proxy/server_test.go
+++ internal/proxy/server_test.go
@@ -5,10 +5,8 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/enix/kube-image-keeper/internal/registry"
 	"github.com/gin-gonic/gin"
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/ghttp"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -36,46 +34,20 @@ func TestNew(t *testing.T) {
 func Test_v2Endpoint(t *testing.T) {
 	g := NewWithT(t)
 
-	// mock server
-	gh := ghttp.NewGHTTPWithGomega(g)
-	server := ghttp.NewServer()
-	defer server.Close()
-	server.AppendHandlers(
-		ghttp.CombineHandlers(
-			gh.VerifyRequest(http.MethodGet, "/v2"),
-			gh.RespondWith(401, nil, http.Header{
-				"Www-Authenticate": []string{
-					"Bearer realm=\"" + server.URL() + "/token\",service=\"registry.docker.io\",scope=\"repository:samalba/my-app:pull,push\"",
-				},
-			}),
-		),
-		ghttp.CombineHandlers(
-			gh.VerifyRequest(http.MethodGet, "/token"),
-			gh.RespondWithJSONEncoded(http.StatusOK, &Bearer{
-				Token:     "token",
-				ExpiresIn: "3600",
-			}),
-		),
-		ghttp.CombineHandlers(
-			gh.VerifyRequest(http.MethodGet, "/v2"),
-			gh.RespondWith(200, "v2 endpoint"),
-		),
-	)
-
 	// mock proxy server
 	recorder := httptest.NewRecorder()
 	ctx, r := gin.CreateTestContext(recorder)
 	proxy := NewWithEngine(dummyK8sClient, r).Serve()
 
 	// mock request
-	registry.Endpoint = server.Addr()
 	request, err := http.NewRequest("GET", "/v2", nil)
 	g.Expect(err).To(BeNil())
 	ctx.Request = request
 	ctx.Writer = &ResponseWriterPatched{ctx.Writer}
 
 	proxy.v2Endpoint(ctx)
-	g.Expect(recorder.Body.String()).To(Equal("v2 endpoint"))
+	g.Expect(recorder.Result().StatusCode).To(Equal(http.StatusOK))
+	g.Expect(recorder.Body.String()).To(Equal("{}"))
 }
 
 func Test_handleOriginRegistryPort(t *testing.T) {
