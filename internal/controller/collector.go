a1e6a0e2eae5a347e7f8d2dc6eacb0470d75c533
Paul Laffitte <paul.laffitte@enix.fr>
2024-06-07 23:47:19 +0200

    feat(metrics): add repositories metric

--- internal/controller/collector.go
+++ internal/controller/collector.go
@@ -54,6 +54,10 @@ var (
 	cachedImagesMetric = prometheus.BuildFQName(kuikMetrics.Namespace, subsystem, "cached_images")
 	cachedImagesHelp   = "Number of images expected to be cached"
 	cachedImagesDesc   = prometheus.NewDesc(cachedImagesMetric, cachedImagesHelp, []string{"status", "cached", "expiring"}, nil)
+
+	repositoriesMetric = prometheus.BuildFQName(kuikMetrics.Namespace, subsystem, "repositories")
+	repositoriesHelp   = "Number of repositories"
+	repositoriesDesc   = prometheus.NewDesc(repositoriesMetric, repositoriesHelp, []string{"status"}, nil)
 )
 
 func RegisterMetrics(client client.Client) {
@@ -76,26 +80,63 @@ type ControllerCollector struct {
 
 func (c *ControllerCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- cachedImagesDesc
+	ch <- repositoriesDesc
 }
 
 func (c *ControllerCollector) Collect(ch chan<- prometheus.Metric) {
-	cachedImageList := &kuikv1alpha1.CachedImageList{}
-	if err := c.List(context.Background(), cachedImageList); err == nil {
-		cachedImageGaugeVec := prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: cachedImagesMetric,
-				Help: cachedImagesHelp,
-			},
-			[]string{"status", "cached", "expiring"},
-		)
-		for _, cachedImage := range cachedImageList.Items {
-			cachedImageGauge := cachedImageGaugeVec.WithLabelValues(cachedImage.Status.Phase, strconv.FormatBool(cachedImage.Status.IsCached), strconv.FormatBool(cachedImage.Spec.ExpiresAt != nil))
-			cachedImageGauge.Inc()
-		}
-		cachedImageGaugeVec.Collect(ch)
+	if cachedImagesGaugeVec, err := c.getCachedImagesMetric(); err == nil {
+		cachedImagesGaugeVec.Collect(ch)
 	} else {
-		log.FromContext(context.TODO()).Error(err, "could not collect "+cachedImagesMetric+" metric")
+		log.FromContext(context.Background()).Error(err, "could not collect "+cachedImagesMetric+" metric")
 	}
+
+	if repositoriesGaugeVec, err := c.getRepositoriesMetric(); err == nil {
+		repositoriesGaugeVec.Collect(ch)
+	} else {
+		log.FromContext(context.Background()).Error(err, "could not collect "+repositoriesMetric+" metric")
+	}
+}
+
+func (c *ControllerCollector) getCachedImagesMetric() (*prometheus.GaugeVec, error) {
+	cachedImageList := &kuikv1alpha1.CachedImageList{}
+	if err := c.List(context.Background(), cachedImageList); err != nil {
+		return nil, err
+	}
+
+	cachedImagesGaugeVec := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: cachedImagesMetric,
+			Help: cachedImagesHelp,
+		},
+		[]string{"status", "cached", "expiring"},
+	)
+	for _, cachedImage := range cachedImageList.Items {
+		cachedImagesGauge := cachedImagesGaugeVec.WithLabelValues(cachedImage.Status.Phase, strconv.FormatBool(cachedImage.Status.IsCached), strconv.FormatBool(cachedImage.Spec.ExpiresAt != nil))
+		cachedImagesGauge.Inc()
+	}
+
+	return cachedImagesGaugeVec, nil
+}
+
+func (c *ControllerCollector) getRepositoriesMetric() (*prometheus.GaugeVec, error) {
+	repositoriesList := &kuikv1alpha1.RepositoryList{}
+	if err := c.List(context.Background(), repositoriesList); err != nil {
+		return nil, err
+	}
+
+	repositoriesGaugeVec := prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: repositoriesMetric,
+			Help: repositoriesHelp,
+		},
+		[]string{"status"},
+	)
+	for _, repository := range repositoriesList.Items {
+		repositoriesGauge := repositoriesGaugeVec.WithLabelValues(repository.Status.Phase)
+		repositoriesGauge.Inc()
+	}
+
+	return repositoriesGaugeVec, nil
 }
 
 func SetLeader(leader bool) {
