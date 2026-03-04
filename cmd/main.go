61e061a78b6ca11691834b76007b567d898f47cc
Paul Laffitte <paul.laffitte@enix.fr>
2026-03-04 14:58:07 +0100

    chore: scaffold ClusterImageSetAvailability CRD+controller

--- cmd/main.go
+++ cmd/main.go
@@ -264,6 +264,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ClusterReplicatedImageSetSecretOwner")
 		os.Exit(1)
 	}
+	if err = (&kuikcontroller.ClusterImageSetAvailabilityReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ClusterImageSetAvailability")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if metricsCertWatcher != nil {
